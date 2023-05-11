191e223e39a569e246e374cfa77e3fc16cc11393
Sanflofer <>
2023-05-11 22:49:45 +0000

    Initial javascript coding

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const squaresContainer = document.querySelector(".squares");
+const colors = ["blue", "yellow", "green", "red", "orange", "purple"];
+const colorsPicklist = [...colors, ...colors]
+const squareCount = colorsPicklist.length;
+
